e685f8d127b5fad36e5faff61621d1f82b6f4e00
mo2men1 <mouemened@gmail.com>
2015-12-16 00:52:40 +0200

    initial method for user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def login
-  	
+  	user = User.find_by(email: params[:user][:email])
+  	if @email && user.authenticate(params[:user][:password])
+  		redirect_to home
+  	else
+  		render "login"
+  	end
   end
 end
