e59bdc9b8653c8406d473f47d06cbb356777fdc2
Mohamed Seleem <seleem1337@gmail.com>
2015-12-19 22:10:21 +0200

    Add model and database validations for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  validates :text, presence: true, length: { minimum: 1 }
+  validates_associated :commentable, :user
 end
