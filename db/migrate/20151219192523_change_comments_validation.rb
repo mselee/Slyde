a115c6ed62fe5e0436446ce0f207f29151c0f4ce
Mohamed Seleem <seleem1337@gmail.com>
2015-12-20 19:03:22 +0200

    Change commentable_id and commentable_type to NON NULL

--- db/migrate/20151219192523_change_comments_validation.rb
+++ db/migrate/20151219192523_change_comments_validation.rb
@@ -2,5 +2,7 @@ class ChangeCommentsValidation < ActiveRecord::Migration
   def change
     change_column :comments, :user_id, :integer, :null => false
     change_column :comments, :text, :text, :null => false
+    change_column :comments, :commentable_id, :integer, :null => false
+    change_column :comments, :commentable_type, :string, :null => false
   end
 end
