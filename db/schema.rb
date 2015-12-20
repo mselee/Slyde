872a42f228edf27cf3b6d6e8c7337e452510ba68
mohamed_mamdouh <mm95mido@gmail.com>
2015-12-20 02:19:36 +0200

    secure password

--- db/schema.rb
+++ db/schema.rb
@@ -34,14 +34,23 @@ ActiveRecord::Schema.define(version: 20151219204504) do
 
   add_index "documents", ["user_id"], name: "index_documents_on_user_id"
 
+  create_table "slides", force: :cascade do |t|
+    t.string   "file_path"
+    t.integer  "document_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "slides", ["document_id"], name: "index_slides_on_document_id"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.integer  "age"
     t.string   "gender"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
