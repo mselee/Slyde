813fdcf4fc7a1fb4c076503998e470025889bd90
mo2men1 <mouemened@gmail.com>
2015-12-20 16:45:15 +0200

    can access signup without logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  skip_before_action :ensure_login, only: [:new, :create]
 
   def create
     @user = User.create!(params.require(:user).permit(:name,:password,:email,:gender))
