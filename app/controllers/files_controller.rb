cac9910ff23aa4ee6dc6226ac75aff96e612bec6
mohamed_mamdouh <mm95mido@gmail.com>
2015-12-22 17:00:14 +0200

    edited path

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,8 +1,8 @@
 class FilesController < ApplicationController
   def images
-  	reader = PDF::Reader.new("/home/cse2017/task2/public/sheet01/Sheet01.pdf")
+  	reader = PDF::Reader.new("public/sheet01/Sheet01.pdf")
 count=reader.page_count
-pdf_file_name = "/home/cse2017/task2/public/sheet01/Sheet01.pdf"
+pdf_file_name = "public/sheet01/Sheet01.pdf"
 im = Magick::Image.read(pdf_file_name)
 
 	for i in 0..count-1
@@ -12,7 +12,7 @@ im = Magick::Image.read(pdf_file_name)
 		x = (im[i].columns - 125) / 2
         y = (im[i].rows - 125) / 2
         im[i] = im[i].extent(125, 125, x, y) 
-        im[i].write("/home/cse2017/task2/public/sheet01/thumbnails/Sheet01.pdf"+"#{i}"+ ".png")
+        im[i].write("public/sheet01/thumbnails/Sheet01.pdf"+"#{i}"+ ".png")
         
   	
    end
