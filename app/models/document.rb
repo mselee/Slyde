66f130e88b2392ab4f4e4c28c8f514b924b63f6a
Mohamed A M-Hassan <anehmr0@gmail.com>
2016-01-30 14:10:20 +0200

    Feature file upload

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,5 @@
 class Document < ActiveRecord::Base
+  mount_uploader :file_path, FileUploader
   belongs_to :user
   has_many :slides, dependent: :delete_all
   has_many :comments, :as => :commentable, dependent: :delete_all
