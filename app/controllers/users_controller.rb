a1a9424b3082d9586a5783d3faeb1fbab73f4a9d
mohamed_mamdouh <mm95mido@gmail.com>
2016-01-31 17:40:54 +0200

    adding age to sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :ensure_login, only: [:new, :create]
 
   def create
-    @user = User.create!(params.require(:user).permit(:name,:password,:email,:gender))
+    @user = User.create!(params.require(:user).permit(:name,:password,:email,:age,:gender))
 
    end
 end
