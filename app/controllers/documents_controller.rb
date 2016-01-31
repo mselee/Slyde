b1901ea642afa0fb388a819d30a908bbe5d4b233
mohamed_mamdouh <mm95mido@gmail.com>
2016-01-31 19:00:43 +0200

    automatically splitting pdf into thumbnails

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -20,6 +20,21 @@ class DocumentsController < ApplicationController
     respond_to do |format|
       if @document.save
         format.html { redirect_to @document, notice: 'Document was successfully created.' }
+        reader = PDF::Reader.new("public/uploads/#{@document.id}/#{@document.name}")
+        count=reader.page_count
+        pdf_file_name = "public/uploads/#{@document.id}/#{@document.name}"
+        im = Magick::Image.read(pdf_file_name)
+
+            for i in 0..count-1
+              im[i].write(pdf_file_name+"#{i}"+ ".jpg")
+              im[i].resize_to_fit(125, 125)
+              im[i].background_color = "#FFFFFF"
+              x = (im[i].columns - 125) / 2
+              y = (im[i].rows - 125) / 2
+              im[i] = im[i].extent(125, 125, x, y) 
+              Dir.mkdir("public/uploads/#{@document.id}/thumbnails")unless File.exists?("public/uploads/#{@document.id}/thumbnails")  
+              im[i].write("public/uploads/#{@document.id}/thumbnails/#{@document.name}"+"#{i}"+".jpg")    
+            end
 
       else
         format.html { render :new }
