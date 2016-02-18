aa962237b9d96600d9c78224d769d5abf8fd5774
Mohamed Seleem <seleem1337@gmail.com>
2016-02-18 17:10:56 +0200

    Fix getting slide by number

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -5,7 +5,7 @@ class SlidesController < ApplicationController
 
   def show
     @document = Document.find(params[:document_id])
-    @slide = @document.slides.find_by(params[:number])
+    @slide = @document.slides.find_by_number(params[:number])
     @index = params[:number].to_i
     @next_slide = @document.slides.exists?(number: @index + 1)
     @prev_slide = @document.slides.exists?(number: @index - 1)
