93c2d022bb2853ab92d90d3417f7a9b9fb5d821f
mo2men1 <mouemened@gmail.com>
2016-02-22 18:05:43 +0200

    Login when new user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,23 @@ class UsersController < ApplicationController
 
   def new
   	if logged_in?
-  		redirect_to '/documents'
+  		if params[:from] = "signup"
+  			render "create"
+  		else
+	  		redirect_to '/documents'
+	  	end
   	end
   end
 
 
   def create
     @user = User.create!(params.require(:user).permit(:name,:password,:email,:age,:gender))
-
+  	parameters = Hash.new
+  	user = Hash.new
+  	user = {"email" => params[:user][:email] , "password" => params[:user][:password] , "new" => true }
+  	parameters = {'user' => user}
+  	# byebug
+    redirect_to login_path(parameters)
+  	# redirect_to controller: "Sessions", action: 'create', param1: user
    end
 end
